0b0b001323f8597180e5c57e824acc0b1d2d96ce
Sem8 <mssemmi8@gmail.com>
2019-08-24 01:20:24 -0400

    wrote the function to allow to check if chain is valid or not by checking previous hash stays same and whether current blocks hash has also stayed the same

--- main.js
+++ main.js
@@ -33,6 +33,23 @@ class BlockChain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let semCoin = new BlockChain();
